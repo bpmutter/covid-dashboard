b129a50a2c2cc539c92682fc74480b4556274ca5
Ben Perlmutter <benperlmutter94@gmail.com>
2020-11-01 17:26:04 -0500

    add basic netlify functions to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import ListIcon from "@material-ui/icons/List";
 
 const useStyles = makeStyles((theme) => ({
@@ -21,9 +20,20 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [data, setData] = React.useState();
+
+  React.useEffect(() => {
+    (async () => {
+      const res = await fetch(`/.netlify/functions/hello-world`);
+      const json = await res.json();
+
+      setData(json);
+    })();
+  }, []);
 
   return (
     <div className={classes.root}>
+      {data && console.log("data is...", data)}
       <AppBar>
         <Toolbar>
           <IconButton
